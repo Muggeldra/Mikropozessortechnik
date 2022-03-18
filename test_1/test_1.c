0e68359c22351662f1c3c3809184aeac2e8351cf
Muggeldra <36767497+Muggeldra@users.noreply.github.com>
2022-03-18 09:56:59 +0100

    LabTest_1 Fertig3
    Waow

--- test_1/test_1.c
+++ test_1/test_1.c
@@ -39,6 +39,120 @@ test_1.c
 #define RGB_Blue 0
 
 int main(void)
+{		
+	GLCD_Init();
+	Switch_Init();
+	LED_Init();
+	
+	GPIOSetDir(OUT_PORT, PORT_PIN, GPIO_OUTPUT);
+	GPIOSetValue(OUT_PORT,PORT_PIN,PORT_PIN_LOW);
+	
+	while(1)
+	{	
+		//prep 1.6
+		GLCD_Simulation();
+		LED_Out(Get_SwitchPos());
+
+	} //end while(1)
+	
+}	// end main()
+#endif
+
+
+//================================================================================
+//  Main-Function Test T1_2
+//================================================================================
+#if (T1_2 == 1)
+
+int main(void)
+{	
+	int TA12StateBefore = 0;
+	int TA11StateBefore = 0;
+	int TA10StateBefore = 0;
+
+	button_Init();
+	Joystick_Init();
+
+	while(1)
+	{
+		//prep 1.7
+		GLCD_Simulation();
+		TA12StateBefore = RGB_FlipFlop(RGB_Red,Get_TA12Stat(),TA12StateBefore);
+		TA10StateBefore = RGB_FlipFlop(RGB_Green,Get_TA10Stat(),TA10StateBefore);
+		TA11StateBefore = RGB_FlipFlop(RGB_Blue,Get_TA11Stat(),TA11StateBefore);
+		
+	} // end while(1)
+}	// end main()
+
+#endif
+
+
+
+//================================================================================
+//  Main-Function Test T1_3
+//================================================================================
+#if (T1_3 == 1)
+
+int main(void)
+{	
+	int LeftStateBefore = 0;
+	int RightStateBefore = 0;
+	int UpStateBefore = 0;
+	int DownStateBefore = 0;
+	int CenterStateBeforeR = 0;
+	int CenterStateBeforeG = 0;
+	int CenterStateBeforeB = 0;
+	
+	Joystick_Init();
+	LED_Init();
+	RGB_Init();
+
+	while(1)
+	{
+		//prep 1.8
+		GLCD_Simulation();
+		LeftStateBefore = LED_FlipFlop(6,Get_LeftStat(),LeftStateBefore);
+		RightStateBefore = LED_FlipFlop(2,Get_RightStat(),RightStateBefore);
+		UpStateBefore = LED_FlipFlop(0,Get_UpStat(),UpStateBefore);
+		DownStateBefore = LED_FlipFlop(4,Get_DownStat(),DownStateBefore);
+		
+		CenterStateBeforeR = RGB_FlipFlop(RGB_Red,Get_CenterStat(),CenterStateBeforeR);
+		CenterStateBeforeG = RGB_FlipFlop(RGB_Green,Get_CenterStat(),CenterStateBeforeG);
+		CenterStateBeforeB = RGB_FlipFlop(RGB_Blue,Get_CenterStat(),CenterStateBeforeB);
+		
+	} // end while(1)
+}	// end main()
+
+#endif
+
+
+
+//================================================================================
+//  Main-Function TestT1_4
+//================================================================================
+#if (T1_4 == 1)
+
+int main(void)
+{	
+	uint8_t walkingLight = 129;
+	LED_Init();
+	
+	while(1)
+	{
+		//prep 1.9
+		GLCD_Simulation();
+		walkingLight = rolchar(walkingLight, 0);
+	} // end while(1)
+}	// end main()
+
+#endif
+
+//================================================================================
+//  Main-Function Test T1_5
+//================================================================================
+#if (T1_5 == 1)
+
+int main(void)
 {	
 	uint8_t walkingLight = 129;
 	unsigned char previousSwitches = 0;
@@ -47,18 +161,6 @@ int main(void)
 	uint32_t joystickDelay = 261;
 	uint32_t joystickTimer = 0;
 	int walkingLightDirection = 0;
-	
-	int TA12StateBefore = 0;
-	int TA11StateBefore = 0;
-	int TA10StateBefore = 0;
-	
-	int LeftStateBefore = 0;
-	int RightStateBefore = 0;
-	int UpStateBefore = 0;
-	int DownStateBefore = 0;
-	int CenterStateBeforeR = 0;
-	int CenterStateBeforeG = 0;
-	int CenterStateBeforeB = 0;
 	
 	GLCD_Init();
 	Switch_Init();
@@ -71,37 +173,11 @@ int main(void)
 	GPIOSetValue(2,0,0);
 	GPIOSetDir(2,1,GPIO_OUTPUT);
 	GPIOSetValue(2,1,0);
-	
-	GPIOSetDir(OUT_PORT, PORT_PIN, GPIO_OUTPUT);
-	GPIOSetValue(OUT_PORT,PORT_PIN,PORT_PIN_LOW);
-	
+
 	while(1)
-	{	
-		
-		GLCD_Simulation();
-		//prep 1.6
-		//LED_Out(Get_SwitchPos());
-		
-		//prep 1.7
-		/*
-		TA12StateBefore = RGB_FlipFlop(RGB_Red,Get_TA12Stat(),TA12StateBefore);
-		TA10StateBefore = RGB_FlipFlop(RGB_Green,Get_TA10Stat(),TA10StateBefore);
-		TA11StateBefore = RGB_FlipFlop(RGB_Blue,Get_TA11Stat(),TA11StateBefore);
-		
-		//prep 1.8
-		LeftStateBefore = LED_FlipFlop(6,Get_LeftStat(),LeftStateBefore);
-		RightStateBefore = LED_FlipFlop(2,Get_RightStat(),RightStateBefore);
-		UpStateBefore = LED_FlipFlop(0,Get_UpStat(),UpStateBefore);
-		DownStateBefore = LED_FlipFlop(4,Get_DownStat(),DownStateBefore);
-		
-		CenterStateBeforeR = RGB_FlipFlop(RGB_Red,Get_CenterStat(),CenterStateBeforeR);
-		CenterStateBeforeG = RGB_FlipFlop(RGB_Green,Get_CenterStat(),CenterStateBeforeG);
-		CenterStateBeforeB = RGB_FlipFlop(RGB_Blue,Get_CenterStat(),CenterStateBeforeB);
-		*/
-		//prep 1.9
-		//walkingLight = rolchar(walkingLight, 0);
-		
+	{
 		//prep 1.10 and 1.11
+		GLCD_Simulation();
 		//value
 		if(previousSwitches != Get_SwitchPos()){
 			previousSwitches = Get_SwitchPos();
@@ -147,76 +223,6 @@ int main(void)
 				RGB_On(RGB_Green);
 			}
 		}
-
-	}
-	
-}	// end main()
-#endif
-
-
-//================================================================================
-//  Main-Function Test T1_2
-//================================================================================
-#if (T1_2 == 1)
-
-int main(void)
-{	
-
-	while(1)
-	{
-		
-	} // end while(1)
-}	// end main()
-
-#endif
-
-
-
-//================================================================================
-//  Main-Function Test T1_3
-//================================================================================
-#if (T1_3 == 1)
-
-int main(void)
-{	
-
-	while(1)
-	{
-		
-	} // end while(1)
-}	// end main()
-
-#endif
-
-
-
-//================================================================================
-//  Main-Function TestT1_4
-//================================================================================
-#if (T1_4 == 1)
-
-int main(void)
-{	
-
-	while(1)
-	{
-		
-	} // end while(1)
-}	// end main()
-
-#endif
-
-//================================================================================
-//  Main-Function Test T1_5
-//================================================================================
-#if (T1_5 == 1)
-
-int main(void)
-{	
-
-	while(1)
-	{
-		
 	} // end while(1)
 }	// end main()
 
