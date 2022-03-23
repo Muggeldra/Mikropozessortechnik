c919f82370bb1fa00dc6366d1c6839b7f2e7dccc
Hammelwade <jeremias.karwey@gmx.net>
2022-03-23 22:08:39 +0100

    test6 start

--- test_6/test_6.c
+++ test_6/test_6.c
@@ -14,7 +14,8 @@ test_6.c
 #include <stdio.h>
 
 #include "tests.h"
-
+#include "delay.h"
+#include "glcd.h"
 
 //================================================================================
 //Definition v. Konstanten:
@@ -27,8 +28,46 @@ test_6.c
 //  test T6_1
 //================================================================================
 #if (T6_1==1)
+
+static unsigned int ticks = 0;
+static unsigned int seconds = 0;
+const unsigned char SegChars[10] = {0x24, 0xAF, 0xE0, 0xA2, 0x2B, 0x32, 0x30, 0xA7, 0x20, 0x22};
+static unsigned char outCode[2];
+
+void SysTick_Handler(void){
+		ticks++;
+	
+		// seconds variable
+		if(!(ticks % 100)){
+			seconds++;
+			if(seconds >= 60){
+				seconds = 0;
+			}
+			// generate output
+			for(int i = 0; i < 2; i++){
+					outCode[i] = SegChars[(seconds >> i)%1];
+			}
+			
+		}
+		
+		
+}
+
 int main(void)
 {	
+	// systick 10ms
+	SystemCoreClockUpdate();
+	SysTick_Config(SystemCoreClock/100);
+	SysTick_Handler();
+	
+	// LCD Init
+	GLCD_Init();
+	GLCD_Clear(White);
+	GLCD_SetBackColor(Blue);
+	GLCD_SetTextColor(Yellow);
+	GLCD_DisplayString(0,3,FONT_16x24,(unsigned char*)"Lab microproc.");
+	GLCD_DisplayString(1,2,FONT_16x24,(unsigned char*)"test2.2 switches");
+	GLCD_DisplayString(2,5,FONT_16x24,(unsigned char*)"Group A.7"); //TO-DO: Set correct group
 
 	while(1)
 	{
