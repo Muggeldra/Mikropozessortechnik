b42e66777516aab83d8458e4973db3a711000e8e
Hammelwade <jeremias.karwey@gmx.net>
2022-04-01 20:05:01 +0200

    Test 6.1

--- test_6/test_6.c
+++ test_6/test_6.c
@@ -16,6 +16,8 @@ test_6.c
 #include "tests.h"
 #include "delay.h"
 #include "glcd.h"
+#include "gpio.h"
+#include "lcd.h"
 
 //================================================================================
 //Definition v. Konstanten:
@@ -32,7 +34,7 @@ test_6.c
 static unsigned int ticks = 0;
 static unsigned int seconds = 0;
 const unsigned char SegChars[10] = {0x24, 0xAF, 0xE0, 0xA2, 0x2B, 0x32, 0x30, 0xA7, 0x20, 0x22};
-static unsigned char outCode[2];
+static unsigned char outLeft, outRight;
 
 void SysTick_Handler(void){
 		ticks++;
@@ -44,13 +46,19 @@ void SysTick_Handler(void){
 				seconds = 0;
 			}
 			// generate output
-			for(int i = 0; i < 2; i++){
-					outCode[i] = SegChars[(seconds >> i)%1];
-			}
-			
+			outLeft = SegChars[(seconds/10)%10];
+			outRight = SegChars[seconds%10];
 		}
-		
-		
+		if(ticks%2){
+			GPIOSetValue(0, 1, 1);
+			LPC_SSP0->DR = outRight;
+			GPIOSetValue(3,25, 0);
+		}
+		else{
+			GPIOSetValue(3,25, 1);
+			LPC_SSP0->DR = outLeft;
+			GPIOSetValue(0, 1, 0);
+		}
 }
 
 int main(void)
@@ -63,15 +71,46 @@ int main(void)
 	// LCD Init
 	GLCD_Init();
 	GLCD_Clear(White);
-	GLCD_SetBackColor(Blue);
+	GLCD_SetBackColor(Maroon);
 	GLCD_SetTextColor(Yellow);
 	GLCD_DisplayString(0,3,FONT_16x24,(unsigned char*)"Lab microproc.");
 	GLCD_DisplayString(1,2,FONT_16x24,(unsigned char*)"test2.2 switches");
 	GLCD_DisplayString(2,5,FONT_16x24,(unsigned char*)"Group A.7"); //TO-DO: Set correct group
+	
+	GLCD_DisplayString(4, 1, FONT_16x24, (unsigned char*)"sec:");
+	GLCD_DisplayString(4, 1, FONT_16x24, (unsigned char*)"out left:");
+	GLCD_DisplayString(4, 1, FONT_16x24, (unsigned char*)"out right:");
+	
+	// SPI Init
+	LPC_PINCON->PINSEL0 &=~ (3<<30); //P0.15
+	LPC_PINCON->PINSEL0 |=  (2<<30);
+	LPC_PINCON->PINSEL1 &=~ 0x3F; //P0.16-18
+	LPC_PINCON->PINSEL1 |=  0x2A;
+	LPC_PINCON->PINMODE1 &=~ (3<<2); //P0.17 neither pull up nor pull down
+	LPC_PINCON->PINMODE1 |=  (2<<2);
+	
+	LPC_SC->PCLKSEL1 &=~ (3<<10); //clk = cclk
+	LPC_SC->PCLKSEL1 |=  (1<<10);
+	
+	LPC_SSP0->CR0 &=~ (0xFFFF); //control register 0
+	LPC_SSP0->CR0 |=  (0x0007);
+	
+	LPC_SSP0->CPSR = 100; //clock prescale
+	
+	LPC_SSP0->CR1 = 2; //control register 1
+	
+	// gpio init
+	GPIOSetDir(0, 1, 1);
+	GPIOSetDir(3,25, 1);
+	
+	GPIOSetValue(0, 1, 1);
+	GPIOSetValue(3,25, 1);
 
 	while(1)
 	{
-		
+		GLCD_DisplayString(4, 1, FONT_16x24, (unsigned char*)lcd_dez(seconds));
+		GLCD_DisplayString(4, 1, FONT_16x24, (unsigned char*)lcd_hex(outLeft));
+		GLCD_DisplayString(4, 1, FONT_16x24, (unsigned char*)lcd_hex(outRight));
 	} // end while(1)
 }	// end main()
 
