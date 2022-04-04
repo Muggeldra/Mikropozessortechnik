c4690c1afe59fbb0629e953869c6dfbb50d73266
Hammelwade <jeremias.karwey@gmx.net>
2022-04-04 08:40:56 +0200

    Test 6.3

--- test_6/test_6.c
+++ test_6/test_6.c
@@ -131,6 +131,17 @@ unsigned char SegChars[10]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
 unsigned char sbuf[3];
 int32_t ret;
 
+int32_t out7seg(uint8_t seg1, uint8_t seg2, uint8_t seg3, uint8_t seg4){
+	uint8_t tx_dat[5];
+	tx_dat[0] = 0x01;
+	tx_dat[1] = SegChars[seg1&0xF]|(seg1&0x80);
+	tx_dat[2] = SegChars[seg2&0xF]|(seg2&0x80);
+	tx_dat[3] = SegChars[seg3&0xF]|(seg3&0x80);
+	tx_dat[4] = SegChars[seg4&0xF]|(seg4&0x80);
+	
+	return I2C1Write(0x38, tx_dat, 5);
+}
+
 void SysTick_Handler(void){
 		ticks++;
 	
@@ -180,12 +191,83 @@ int main(void)
 //================================================================================
 #if (T6_3==1)
 
+unsigned int ticks = 0;
+unsigned int seconds = 0;
+unsigned char SegChars[10]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};
+unsigned char sbuf[3];
+int32_t ret;
+int32_t temp;
+
+int32_t out7seg(uint8_t seg1, uint8_t seg2, uint8_t seg3, uint8_t seg4){
+	uint8_t tx_dat[5];
+	tx_dat[0] = 0x01;
+	tx_dat[1] = SegChars[seg1&0xF]|(seg1&0x80);
+	tx_dat[2] = SegChars[seg2&0xF]|(seg2&0x80);
+	tx_dat[3] = SegChars[seg3&0xF]|(seg3&0x80);
+	tx_dat[4] = SegChars[seg4&0xF]|(seg4&0x80);
+	
+	return I2C1Write(0x38, tx_dat, 5);
+}
+
+void SysTick_Handler(void){
+		ticks++;
+	/*
+		// seconds variable
+		if(!(ticks % 100)){
+			seconds++;
+			if(seconds >= 60){
+				seconds = 0;
+			}
+		}*/
+}
+
 int main(void)
 {	
-
+	// systick 10ms
+	SystemCoreClockUpdate();
+	SysTick_Config(SystemCoreClock/100);
+	SysTick_Handler();
+	
+	// LCD Init
+	GLCD_Init();
+	GLCD_Clear(White);
+	GLCD_SetBackColor(Maroon);
+	GLCD_SetTextColor(Yellow);
+	GLCD_DisplayString(0,3,FONT_16x24,(unsigned char*)"Lab microproc.");
+	GLCD_DisplayString(1,2,FONT_16x24,(unsigned char*)"test2.2 switches");
+	GLCD_DisplayString(2,5,FONT_16x24,(unsigned char*)"Group A.7"); //TO-DO: Set correct group
+	
+	GLCD_DisplayString(4, 1, FONT_16x24, (unsigned char*)"sec:");
+	GLCD_DisplayString(5, 1, FONT_16x24, (unsigned char*)"out bytes:");
+	
+	//i2c init
+	I2C1_Init();sbuf[0]=0;sbuf[1]=0x17;//initialize 7 segment display, sbuf[] is to define
+	ret=I2C1Write(0x38,sbuf,2); //it is only possible with connected hardware
+	
+	//temp sensor init
+	sbuf[0]=1;sbuf[1]=0x60; ret=I2C1Write(0x48,sbuf,2);//sbuf[] is to define
+	sbuf[0]=0;
+	
 	while(1)
 	{
-		
+		if(!sbuf[0]){
+			ret=I2C1Write(0x48,sbuf,1); //sbuf[] is to define
+		}
+		if(!ret){
+			ret=I2C1Read(0x48,sbuf,2);
+			if(!ret){
+				temp = sbuf[0]|sbuf[1];
+				sbuf[0]=0;
+			}
+		}
+		if(!temp){
+			temp *= 625;
+			temp /= 100;
+			
+			out7seg((temp/1000)%10, (temp/100)%10, (temp/10)%10, temp%10);
+			
+			temp=0;
+		}
 	} // end while(1)
 }	// end main()
 
